b6d23466ded3d2c17bdd8f654edfd20198433df2
Aripaka Vamsy Kiran <a.vamc.it@gmail.com>
2021-09-09 15:24:00 +0530

    day15

--- ang-app06/src/app/shared/registration/registration.component.ts
+++ ang-app06/src/app/shared/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/service/users.service';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  name:string;
+  email:string;
+  pwd:string;
+  err?:string;
+  msg?:string;
+
+  constructor(private userService:UsersService) {
+    this.name='';
+    this.email='';
+    this.pwd='';
+  }
 
   ngOnInit(): void {
   }
 
+  register(){
+    this.userService.add({name:this.name,email:this.email,password:this.pwd,role:'USER'}).subscribe(
+      user => {
+        this.msg="User Registered Successfully with id: "+user.id;
+        this.name='';
+        this.email='';
+        this.pwd='';
+        this.err=undefined;
+      },
+      err => {this.err=err;console.log(this.err);}
+    );
+  }
 }
